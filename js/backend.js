0b2ec63d6798736253c4055e21495a902280fa9c
Rohan Bavishi <rbavishi@berkeley.edu>
2019-05-22 01:06:41 -0700

    trying https backend

--- js/backend.js
+++ js/backend.js
@@ -15,7 +15,7 @@ function ajax_lambda(payload) {
 function ajax_engine(payload) {
     return $.ajax({
         type: "POST",
-        url: "http://128.32.35.240:8000/autopandas",
+        url: "https://dpl.cs.berkeley.edu:8000/autopandas",
         data: JSON.stringify(payload),
         contentType: "application/json; charset=utf-8",
         dataType: "json"
