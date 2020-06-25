db600ec6614d4e40da9e2ce60f9cf43464946f5f
Rohan Bavishi <rbavishi@berkeley.edu>
2020-06-24 17:30:20 -0700

    Adding precise package versions

--- setup.py
+++ setup.py
@@ -14,6 +14,16 @@ setup(
     include_package_data=True,
     entry_points={'console_scripts': ['autopandas_v2=autopandas_v2.main:run_console']},
     zip_safe=False,
-    install_requires=['pebble', 'astor', 'astunparse', 'orderedset', 'pandas==0.23.4', 'tqdm', 'PyYAML',
-                      'tensorflow-gpu==1.9.0']
+    install_requires=[
+        "orderedset==2.0.1",
+        "Pebble==4.4.0",
+        "numpy==1.17.3",
+        "astor==0.8.0",
+        "tensorflow_gpu==1.9.0",
+        "astunparse==1.6.2",
+        "pandas==0.23.4",
+        "tqdm==4.36.1",
+        "python_dateutil==2.8.1",
+        "PyYAML==5.3.1",
+    ],
 )
